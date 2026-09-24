a469b99a64f9409d4763738c2e895075c1d871e2
Devanshgulia <devanshgulia4017@gmail.com>
2026-09-25 01:30:13 +0530

    Time: 4 ms (6.13%), Space: 19.1 MB (17.36%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-
+        
+        chars.push_back('~');
         string s ="";
         int n = chars.size();
         int count = 0;
@@ -10,7 +11,6 @@ public:
 
             if(chars[i+1] != chars[i]){
                 count ++;
-
                 if(count != 1){
                     s = s+chars[i] + to_string(count);
                     count = 0;
@@ -22,20 +22,9 @@ public:
             }
             else{
                 count ++;
-
             }
         }
-        cout << s << count <<  endl;
-        count ++;
-        if(count != 1){
-            s = s+chars[n-1] + to_string(count);
-            count = 0;
-        }
-        else{
-            s+=chars[n-1];
-            count = 0;
-        }
-        cout << s << count <<  endl;
+
         for(int i=0;i<s.size();i++){
             chars[i] = s[i];
         }
